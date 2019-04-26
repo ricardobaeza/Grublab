740437f5be21b38636c119735072db3c8636e3ce
Ricardo Baeza <baezaric3600@gmail.com>
2019-04-26 12:32:29 -0600

    commiting my branch

--- myApp/src/app/tab1/tab1.page.ts
+++ myApp/src/app/tab1/tab1.page.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
-
+import { OnInit } from '@angular/core';
+import { HttpServiceService} from "../shared/services/http-service.service"
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {}
+export class Tab1Page implements OnInit {
+  constructor(private httpService: HttpServiceService) {
+
+  }
+  ngOnInit() {
+    console.log('this is working');
+    this.httpService.getRestuarant("16774318").subscribe(data => {console.log(data)});
+  }
+}
